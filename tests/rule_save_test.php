316a25e6b1749d17015deeba8406693fe2a263c8
kx-coder-01 <you@example.com>
2021-06-28 19:45:58 +0800

    step8

--- tests/rule_save_test.php
+++ tests/rule_save_test.php
@@ -10,6 +10,13 @@ require __DIR__ . '/../vendor/autoload.php';
 $e = new \FastElephant\EventHandler\EventHandler(10000100, '123456');
 
 
-$e->rule()->save(1, [['key'=>'222','value'=>'3333']]);
+$rules = [
+    ['capacity_type' => 1, 'gratuity' => 1, 'delay' => 60],
+    ['capacity_type' => 2, 'gratuity' => 2, 'delay' => 120],
+    ['capacity_type' => 3, 'gratuity' => 3, 'delay' => 30],
+    ['capacity_type' => 4, 'gratuity' => 4, 'delay' => 180],
+];
+
+$e->rule()->save(1, $rules);
 
 
