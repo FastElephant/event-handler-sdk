2189e944acc704a5a414b288ce0ed129e0680370
kx-coder-01 <you@example.com>
2021-06-29 20:15:33 +0800

    u

--- tests/event_dispatch_test.php
+++ tests/event_dispatch_test.php
@@ -8,14 +8,20 @@
 require __DIR__ . '/../vendor/autoload.php';
 
 
-/*$e1 = new \FastElephant\EventHandler\EventHandler();
+$e1 = new \FastElephant\EventHandler\EventHandler();
 
-print_r($e1->event()->dispatch(['url' => '', 'body' => [], 'method' => 'post', 'header' => [], 'delay' => 5]));*/
+$d1 = [
+    ['url' => 'https://psapi.songjianxia.com/event-callback', 'body' => ['name'=>'王'], 'method' => 'get', 'header' => ['Content-Type'=>'application/x-www-form-urlencoded'], 'delay' => 5]
+];
+
+$e1->event()->dispatch($d1);
+
+exit;
 
 $e2 = new \FastElephant\EventHandler\EventHandler(10000100);
 
-$d = [
-    ['delivery_order_id' => 18000111, 'capacity_types' => [1, 4], 'merchant_id' => 123455],
+$d2 = [
+    ['delivery_order_id' => 18000111, 'capacity_types' => [1, 4], 'merchant_id' => 123456],
 ];
 
-print_r($e2->event()->dispatch($d));
+$e2->event()->dispatch($d2);
